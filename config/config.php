7dc4562d1749069fbe219ca6a6e673f60a2a5221
prasha18 <prashaseltha2818@gmail.com>
2024-03-12 17:34:54 +0530

    first commit

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+              define('DB_HOST','localhost');
+              define('DB_USER','root');
+              define('DB_PASS','A.7rD=9(eUf)}V8o');
+              define('DB_NAME','spamdoctor');
+              ?>
